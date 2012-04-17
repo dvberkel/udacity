88efc41bf17e64de7e2b3009023f56aad7f7b472
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-17 20:32:29 +0200

    FSM quiz
    The FSM quiz is solved.

--- cs262/unit-01/code/fsm.py
+++ cs262/unit-01/code/fsm.py
@@ -5,6 +5,10 @@ def fsmsim(string, current, edges, accepting):
         letter = string[0]
         # Quiz: You fill this out
         # Is there a valid edge?
+        if (current, letter) in edges:
         # If so take it
+            return fsmsim(string[1:], edges[(current, letter)], edges, accepting)
         # If not return False
+        else:
+            return False
         # Hint: recursion
